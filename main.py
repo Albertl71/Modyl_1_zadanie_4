95963951163a05ffae693be4f487252bf4a2e1b0
Albert12091971 <flint0309@gmail.com>
2024-12-13 22:06:06 +0300

    Модуль 1 задание 4

---
+++ main.py
@@ -0,0 +1,8 @@
+my_string = "Albert"
+print(len(my_string))
+print({my_string.upper()})
+print({my_string.lower()})
+my_string_without_spaces = my_string.replace(" ", "")
+print(my_string_without_spaces)
+print( {my_string[0]})
+print( {my_string[-1]})
